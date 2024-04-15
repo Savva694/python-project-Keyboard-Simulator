56c6043133acc3c776e3083e4c22eec9ef5bd81d
Chuev Savva <savvachuev@gmail.com>
2024-04-15 15:14:06 +0300

    added stats aware input line_edit and lvl texts

--- main.py
+++ main.py
@@ -4,39 +4,66 @@ import sys
 import json
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout
+import time
 
-# Создание json с пустой статистикой
-stats = {"Пройдено уровней": 0,
-         "Вероятность ошибки": 0,
-         "Количество введённых символов": 0,
-         "Количество ошибок": 0,
-         "Наиболее частые ошибки": 0,
-         "Ср. время набора символа": 0,
-         "Затрачено времени в сумме": 0}
-for i in range(32):
-    stats["Ошибок на букве " + chr(ord("а") + i)] = 0
-    stats["Верных нажатий на букве " + chr(ord("а") + i)] = 0
-    stats["Ошибок на букве " + chr(ord("А") + i)] = 0
-    stats["Верных нажатий на букве " + chr(ord("А") + i)] = 0
-with open('STATS.txt', 'w') as outfile:
-    json.dump(stats, outfile)
+
+# # Создание json с пустой статистикой
+# stats = {"Пройдено уровней": 0,
+#          "Вероятность ошибки": 0,
+#          "Наиболее частые ошибки": 0,
+#          "Ср. время набора символа": 0,
+#          "Количество введённых символов": 0,
+#          "Количество ошибок": 0,
+#          "Затрачено времени в сумме": 0}
+#
+# for i in range(32):
+#     stats["Ошибок на букве " + chr(ord("А") + i)] = 0
+#     stats["Верных нажатий на букве " + chr(ord("А") + i)] = 0
+#
+# for i in range(ord("Z") - ord("A")):
+#     stats["Ошибок на букве " + chr(ord("A") + i)] = 0
+#     stats["Верных нажатий на букве " + chr(ord("A") + i)] = 0
+#
+# with open('STATS.txt', 'w') as outfile:
+#     json.dump(stats, outfile)
 
 
 # Класс для перехвата вводимых символов
 class CustomLineEdit(QLineEdit):
-    def __init__(self, level_text, parent=None):
+    def __init__(self, level_info, parent=None):
         super(CustomLineEdit, self).__init__(parent)
-        self.level_text = level_text
-        self.now_position = 0
-        self.end_position = len(level_text)
+        self.level_info = level_info
 
     def keyPressEvent(self, event):
-        if self.now_position != self.end_position:
+        if (not self.level_info[3]):
+            return
+        if self.level_info[0] != len(self.level_info[1]):
             key = event.key()
-            print(key, chr(key))
-            if chr(key) == (self.level_text[self.now_position]).title():
-                self.now_position += 1
+            if not (0 <= key <= 1114111):
+                return
+            if chr(key) == (self.level_info[1][self.level_info[0]]).title():
+
+                if ord("А") <= key <= ord("Я"):
+                    self.level_info[4]["Верных нажатий на букве " + chr(key)] += 1
+                if ord("A") <= key <= ord("Z"):
+                    self.level_info[4]["Верных нажатий на букве " + chr(key)] += 1
+                self.level_info[4]["Количество введённых символов"] += 1
+
+                self.level_info[0] += 1
+                if self.level_info[0] < 25:
+                    self.level_info[2].setText(
+                        (self.level_info[1] + 50 * " ")[0:self.level_info[0] + 50])
+                else:
+                    self.level_info[2].setText(
+                        (self.level_info[1] + 50 * " ")[self.level_info[0] - 24:self.level_info[0] + 26])
                 super().keyPressEvent(event)
+            else:
+                if ord("А") <= key <= ord("Я"):
+                    self.level_info[4]["Количество ошибок"] += 1
+                    self.level_info[4]["Ошибок на букве " + chr(key)] += 1
+                if ord("A") <= key <= ord("Z"):
+                    self.level_info[4]["Количество ошибок"] += 1
+                    self.level_info[4]["Ошибок на букве " + chr(key)] += 1
 
 
 class MainWindow(QMainWindow):
@@ -68,21 +95,39 @@ class MainWindow(QMainWindow):
         self.btn_start_level_height = 50
         self.btn_start_level_start_width = self.btn_sep
         self.btn_start_level_start_height = self.btn_level_start_height + self.btn_sep * 2 + (
-                self.btn_sep + self.btn_level_height) * ((
-                                                                 self.levels_count + self.btn_level_in_row_count - 1) // self.btn_level_in_row_count)
+                self.btn_sep + self.btn_level_height) * ((self.levels_count + self.btn_level_in_row_count - 1) //
+                                                         self.btn_level_in_row_count)
 
         self.btn_finish_level_width = 100
         self.btn_finish_level_height = 50
         self.btn_finish_level_start_width = self.btn_sep * 4 + self.btn_start_level_width
         self.btn_finish_level_start_height = self.btn_level_start_height + self.btn_sep * 2 + (
-                self.btn_sep + self.btn_level_height) * ((
-                                                                 self.levels_count + self.btn_level_in_row_count - 1) // self.btn_level_in_row_count)
+                self.btn_sep + self.btn_level_height) * ((self.levels_count + self.btn_level_in_row_count - 1) //
+                                                         self.btn_level_in_row_count)
+
+        self.level_text_width = 400
+        self.level_text_height = 25
+        self.level_text_start_width = self.btn_sep + 7
+        self.level_text_start_height = (self.btn_finish_level_start_height + self.btn_finish_level_height +
+                                        self.btn_sep)
+
+        self.line_edit_input_text_width = 190
+        self.line_edit_input_text_height = 25
+        self.line_edit_input_text_start_width = self.level_text_start_width
+        self.line_edit_input_text_start_height = (self.level_text_start_height + self.level_text_height)
+
+        self.cle_values = [0, 0, 0, False, 0]
+
+        self.cle_values[0] = 0
+        self.cle_values[3] = False
+        self.now_level_number = 0
+        self.now_time = 0
 
         # Отрисовка главного окна и его виджетов
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("Keyboard Simulator")
-        self.setGeometry(650, 250, 600, 600)
+        self.setGeometry(650, 250, 610, 600)
         self.show()
 
         # Кнопка показа уровней
@@ -102,29 +147,29 @@ class MainWindow(QMainWindow):
             self.levels[-1].show()
 
         # Кнопка показа статистики
-        self.btn_stats = QPushButton("Show Stats", self)
+        self.btn_stats = QPushButton("Update Stats", self)
         self.btn_stats.resize(self.btn_stats_width, self.btn_stats_height)
         self.btn_stats.move(self.btn_stats_start_width, self.btn_stats_start_height)
-        self.btn_stats.clicked.connect(self.show_or_hide_stats)
+        self.btn_stats.clicked.connect(self.update_stats)
         self.btn_stats.show()
+
+        # Кнопка сброса статистики
+        self.btn_reset_stats = QPushButton("Reset Stats", self)
+        self.btn_reset_stats.resize(self.btn_stats_width, self.btn_stats_height)
+        self.btn_reset_stats.move(self.btn_stats_start_width + self.btn_stats_width + self.btn_sep,
+                                  self.btn_stats_start_height)
+        self.btn_reset_stats.clicked.connect(self.reset_stats)
+        self.btn_reset_stats.show()
 
         # Чтение статистики из json файла
         with open('STATS.txt') as json_stats:
-            self.stats = json.load(json_stats)
-        # for i in self.stats.keys():
-        #     print(i + " " + str(self.stats[i]))
+            self.cle_values[4] = json.load(json_stats)
 
         self.text_stats = QLabel("none", self)
-        self.text_stats.setText("Пройдено уровней:  " + str(self.stats["Пройдено уровней"]) + "\n")
-        self.text_stats.setText(
-            self.text_stats.text() + "Вероятность ошибки:  " + str(self.stats["Вероятность ошибки"]) + "\n")
-        self.text_stats.setText(
-            self.text_stats.text() + "Наиболее частые ошибки:  " + str(self.stats["Наиболее частые ошибки"]) + "\n")
-        self.text_stats.setText(
-            self.text_stats.text() + "Ср. время набора символа:  " + str(self.stats["Ср. время набора символа"]) + "\n")
+        self.update_stats()
 
         self.text_stats.resize(self.text_stats_width, self.text_stats_height)
-        self.text_stats.move(-1000, -1000)
+        self.text_stats.move(self.text_stats_start_width, self.text_stats_start_height)
         self.text_stats.show()
 
         # Кнопка старта уровня
@@ -141,10 +186,17 @@ class MainWindow(QMainWindow):
         self.btn_finish_level.clicked.connect(self.finish_level)
         self.btn_finish_level.show()
 
+        # Текст уровня
+        self.cle_values[2] = QLabel("none", self)
+        self.cle_values[2].setText("Тут должен быть текст уровня...")
+        self.cle_values[2].resize(self.level_text_width, self.level_text_height)
+        self.cle_values[2].move(-1000, -1000)
+        self.cle_values[2].show()
+
         # Поле ввода
-        self.input_text = CustomLineEdit("aboba", self)
-        self.input_text.resize(500, 100)
-        self.input_text.move(self.btn_finish_level_start_width, self.btn_finish_level_start_height)
+        self.input_text = CustomLineEdit(self.cle_values, self)
+        self.input_text.resize(self.line_edit_input_text_width, self.line_edit_input_text_height)
+        self.input_text.move(-1000, -1000)
         self.input_text.show()
 
     def show_or_hide_levels(self):
@@ -164,22 +216,98 @@ class MainWindow(QMainWindow):
 
     def view_level(self):
         button = QApplication.instance().sender()
-        lvl_number = int(button.text().split()[-1])
-        print(lvl_number)
+        self.now_level_number = button.text().split()[-1]
+        self.cle_values[0] = 0
+        self.cle_values[2].move(self.level_text_start_width, self.level_text_start_height)
+        self.input_text.move(self.line_edit_input_text_start_width, self.line_edit_input_text_start_height)
 
-    def show_or_hide_stats(self):
-        if self.btn_stats.text() == "Show Stats":
-            self.btn_stats.setText("Hide Stats")
-            self.text_stats.move(self.text_stats_start_width, self.text_stats_start_height)
-        else:
-            self.btn_stats.setText("Show Stats")
-            self.text_stats.move(-1000, -1000)
+        filename = "LEVELS/LEVEL_" + self.now_level_number + ".txt"
+        with open(filename, "r") as file:
+            self.cle_values[1] = file.read()
+        self.cle_values[2].setText(
+            (self.cle_values[1] + 50 * " ")[0:self.cle_values[0] + 100])
+
+    def update_stats(self):
+        self.text_stats.setText("Пройдено уровней:  " + str(self.cle_values[4]["Пройдено уровней"]) + "\n")
+        self.text_stats.setText(
+            self.text_stats.text() + "Вероятность ошибки:  " + str(
+                int(self.cle_values[4]["Вероятность ошибки"])) + "%\n")
+        self.text_stats.setText(
+            self.text_stats.text() + "Наиболее частые ошибки:  " + str(
+                self.cle_values[4]["Наиболее частые ошибки"]) + "\n")
+        self.text_stats.setText(
+            self.text_stats.text() + "Ср. время набора символа:  " + str(
+                round(self.cle_values[4]["Ср. время набора символа"], 2)) + " c.\n")
+
+        with open('STATS.txt', 'w') as outfile:
+            json.dump(self.cle_values[4], outfile)
+
+    def reset_stats(self):
+        stats = {"Пройдено уровней": 0,
+                 "Вероятность ошибки": 0,
+                 "Наиболее частые ошибки": "-",
+                 "Ср. время набора символа": 0,
+                 "Количество введённых символов": 0,
+                 "Количество ошибок": 0,
+                 "Затрачено времени в сумме": 0}
+
+        for i in range(32):
+            stats["Ошибок на букве " + chr(ord("А") + i)] = 0
+            stats["Верных нажатий на букве " + chr(ord("А") + i)] = 0
+
+        for i in range(ord("Z") - ord("A")):
+            stats["Ошибок на букве " + chr(ord("A") + i)] = 0
+            stats["Верных нажатий на букве " + chr(ord("A") + i)] = 0
+
+        with open('STATS.txt', 'w') as outfile:
+            json.dump(stats, outfile)
+
+        with open('STATS.txt') as json_stats:
+            self.cle_values[4] = json.load(json_stats)
+
+        self.update_stats()
 
     def start_level(self):
-        print("Level started")
+        if int(self.now_level_number) < 1 or int(self.now_level_number) > self.levels_count:
+            return
+        self.cle_values[3] = True
+        self.now_time = time.time()
 
     def finish_level(self):
-        print("Level finished")
+        self.now_time = time.time() - self.now_time
+        self.cle_values[0] = 0
+        self.cle_values[2].move(-1000, -1000)
+        self.cle_values[3] = False
+
+        self.cle_values[4]["Пройдено уровней"] += 1
+        self.cle_values[4]["Затрачено времени в сумме"] += self.now_time
+        self.cle_values[4]["Ср. время набора символа"] = (self.cle_values[4]["Затрачено времени в сумме"] /
+                                                          self.cle_values[4]["Количество введённых символов"])
+        self.cle_values[4]["Вероятность ошибки"] = (self.cle_values[4]["Количество ошибок"] /
+                                                    (self.cle_values[4]["Количество введённых символов"] +
+                                                     self.cle_values[4]["Количество ошибок"])) * 100
+
+        best_mistakes = ["-", -1]
+        for i in range(32):
+            mistakes = self.cle_values[4]["Ошибок на букве " + chr(ord("А") + i)] / (
+                    self.cle_values[4]["Ошибок на букве " + chr(ord("А") + i)] + self.cle_values[4][
+                "Верных нажатий на букве " + chr(ord("А") + i)] + 1)
+            if (mistakes > best_mistakes[1]):
+                best_mistakes = [chr(ord("А") + i) + " (ru)", mistakes]
+
+        for i in range(ord("Z") - ord("A")):
+            mistakes = self.cle_values[4]["Ошибок на букве " + chr(ord("A") + i)] / (
+                    self.cle_values[4]["Ошибок на букве " + chr(ord("A") + i)] + self.cle_values[4][
+                "Верных нажатий на букве " + chr(ord("A") + i)] + 1)
+            if (mistakes > best_mistakes[1]):
+                best_mistakes = [chr(ord("A") + i) + " (en)", mistakes]
+
+        if best_mistakes[1] != 0:
+            self.cle_values[4]["Наиболее частые ошибки"] = best_mistakes[0]
+
+        self.input_text.setText("")
+        self.input_text.move(-1000, -1000)
+        self.now_level_number = 0
 
 
 app = QApplication(sys.argv)
